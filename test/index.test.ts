03a75efda0f0948ab8e07a5f38076d75ce677302
relliv <epsilonelittr@gmail.com>
2024-11-11 17:07:53 +0300

    Add netscape parser test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,27 @@
+// node
+import fs from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+// test
 import { describe, expect, it } from 'vitest'
+import { NetscapeBookmarkParser } from '../src/index'
 
 describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+  it('export netscape bookmark correctly from file', () => {
+    const parser = new NetscapeBookmarkParser()
+
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+
+    const bookmarkFilePath = path.resolve(__dirname, '../example/example_bookmark_export.html')
+    const bookmarkExportJsonPath = path.resolve(__dirname, '../example/example_bookmark_export.json')
+    
+    const bookmarkFileContent = fs.readFileSync(bookmarkFilePath, 'utf-8')
+    const bookmarExportJsonContent = JSON.parse(fs.readFileSync(bookmarkExportJsonPath, 'utf-8'))
+
+    const tree = parser.parse(bookmarkFileContent)
+
+    expect(tree).toEqual(bookmarExportJsonContent)
   })
 })
