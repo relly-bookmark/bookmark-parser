54758c39fbfd691282015052baf0907a8cf05da3
relliv <epsilonelittr@gmail.com>
2024-11-11 17:14:16 +0300

    Add node ray for debugging

--- example/node.ts
+++ example/node.ts
@@ -2,6 +2,7 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import NetscapeBookmarkParser from '../src/parsers/netscape-bookmark-parser'
+import { ray } from 'node-ray';
 
 const parser = new NetscapeBookmarkParser()
 
@@ -11,5 +12,6 @@ const bookmarkFilePath = path.resolve(__dirname, './example_bookmark_export.html
 const bookmarkFileContent = fs.readFileSync(bookmarkFilePath, 'utf-8')
 
 const tree = parser.parse(bookmarkFileContent)
-
 fs.writeFileSync(path.resolve(__dirname, './example_bookmark_export.json'), JSON.stringify(tree, null, 2))
+
+ray(tree).blue();
